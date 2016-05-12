b1f80861d848cf259e78751208b7aafe04a61701
Breezewish <me@breeswish.org>
2016-05-12 10:31:04 +0800

    escape

--- ui/config/translation.js
+++ ui/config/translation.js
@@ -3,6 +3,7 @@ import localeZH from 'locales/zh.json';
 export default function translation ($translateProvider) {
   $translateProvider.translations('zh', localeZH);
   $translateProvider.preferredLanguage('zh');
+  $translateProvider.useSanitizeValueStrategy('escape');
 }
 
 translation.$inject = ['$translateProvider'];
